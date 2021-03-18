10ec24fb15df24f72549a927d9981caadaf916ec
Anibal Ortega Piedrahita <anibalortegap@gmail.com>
2021-03-18 16:24:18 -0500

    Add images folder, change favicon, change name selector for AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'pm-root',
+  template: `
+    <div>
+      <h1>{{ pageTitle }}</h1>
+      <div>My First Component</div>
+    </div>
+  `,
 })
 export class AppComponent {
-  title = 'APM';
+  pageTitle: string = 'Acme Products Management';
 }
